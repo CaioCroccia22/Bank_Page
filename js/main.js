994a41060f7cd1adb9c307e2a11c795cb04e2a2f
caiocroccia22 <caiocroccia22@gmail.com>
2024-08-05 21:41:51 -0300

    feat: add validation for beneficiary name in the deposit form

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
-const form = document.getElementById('form-deposito')
+const form = document.getElementById('form-deposito');
 
-console.log(form)
+function validaNome(nomeCompleto){
+    const NomeComoArray = nomeCompleto.split(' ');
+    return NomeComoArray.length >= 2;
+};
+
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    const nomeBeneficiario = document.getElementById('nome_beneficiario');
+    // console.log(nomeBeneficiario.value)
+    if (!validaNome(nomeBeneficiario.value)){
+        alert('Nome não esta completo');
+    }
+    else{
+        alert('Nome esta completo')
+    }
+});
+
+console.log(form);
 //alert('Olá mundo!!')
